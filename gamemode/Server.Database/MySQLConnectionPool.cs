ea44b970b4bb2c4b6627fbecdd424d1d7ba1c9fe
Raphael Santos <raphaelmacsa@gmail.com>
2020-07-22 02:33:43 -0300

    Move csharp solution completely into gamemode folder

--- gamemode/Server.Database/MySQLConnectionPool.cs
+++ gamemode/Server.Database/MySQLConnectionPool.cs
@@ -10,21 +10,24 @@ namespace Server.Database
 
         public MySqlConnectionPool(int poolSize)
         {
+            Console.WriteLine($"[IM MySqlConnectionPool] Starting database connection pool with size: {poolSize}");
             var connectionString = "Server=192.168.99.100; Database=ilhadamagia; Uid=root; Pwd=PQPMELANCIAAZEDA;";
             this.availableConnections = new List<MySqlConnection>();
-
             for (int i = 0; i < poolSize; i++)
             {
                 var connection = new MySqlConnection(connectionString);
                 connection.Open();
                 this.availableConnections.Add(connection);
+                Console.WriteLine($"[IM MySqlConnectionPool] Started pool connection #{i}");
             }
+            Console.WriteLine($"[IM MySqlConnectionPool] Database connection pool started successful with size: {poolSize}");
         }
 
         public MySqlConnection GetOne()
         {
             var random = new Random();
             var randomIndex = random.Next(this.availableConnections.Count);
+            Console.WriteLine($"[IM MySqlConnectionPool] Database connection in use: #{randomIndex}");
             return this.availableConnections[randomIndex];
         }
     }
