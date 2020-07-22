dca948b68e0ce078381de61a5786a0dc32a77009
Raphael Santos <raphaelmacsa@gmail.com>
2020-07-22 01:58:59 -0300

    Move files to new solution structure

--- scripts/run_BUILD.bat
+++ scripts/run_BUILD.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" C:\Users\jogos\source\repos\Client\GF.CrossCutting\GF.CrossCutting.csproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" C:\Users\jogos\source\repos\Client\Shared.CrossCutting\Shared.CrossCutting.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" C:\Users\jogos\source\repos\Client\Client\Client.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" C:\Users\jogos\source\repos\Client\Server\Server.csproj
